40123858efe8d0baef3667219f674cd2757a5651
mawross <info@qsamax.org>
2023-09-01 11:31:48 +0200

    Pierwszy Commit

---
+++ src/main/java/git/spring6webapp1/Spring6Webapp1Application.java
@@ -0,0 +1,13 @@
+package git.spring6webapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6Webapp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6Webapp1Application.class, args);
+    }
+
+}
---
+++ src/test/java/git/spring6webapp1/Spring6Webapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package git.spring6webapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6Webapp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
